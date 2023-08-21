cb24aa116b0f3cb2ec7f76052aac80e96e78cb28
shahin norouzi <37897967+sir-shahin@users.noreply.github.com>
2023-08-21 12:25:45 +0330

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["https://sir-shahin.github.io/how-it-work"]
-  },
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
